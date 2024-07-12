d1cde44e02eab4f5acfbb3effb5b067567bf1dbd
IMarcellinus <ignacymischa@gmail.com>
2024-07-12 21:14:22 +0700

    removing page Add,Blog,and Edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,18 @@ import { Route, Routes, useNavigate, NavLink } from "react-router-dom";
 import { FetchUser } from "../services/store/reducers/Authslice";
 import "./index.css";
 import Layout from "./layout/Layout";
-import Add from "./pages/Add";
 import ChangePassword from "./pages/Auth/ChangePassword";
 import Login from "./pages/Auth/Login";
 import LoginUserPage from "./pages/Auth/LoginUser";
 import RegisterUserPage from "./pages/Auth/RegisterUserPage";
-import Blog from "./pages/Blog";
 import BookPage from "./pages/Book/BookPage";
 import PeminjamanPage from "./pages/Book/PeminjamanPage";
 import PengembalianPage from "./pages/Book/PengembalianPage";
 import DashboardPage from "./pages/DashboardPage";
-import Edit from "./pages/Edit";
 import PageNotFound from "./pages/PageNotFound";
 import UserPage from "./pages/User/UserPage";
 import Swal from "sweetalert2";
+import ReservationPage from "./pages/Reservation/ReservationPage";
 
 function App() {
   const {
@@ -79,8 +77,9 @@ function App() {
           {/* Gunakan Outlet untuk menampilkan komponen-komponen di bawahnya */}
           {authUser && authUser.role === "admin" && (
             <>
-              <Route path="/user" element={<UserPage authUser={authUser} />} />
               <Route index element={<DashboardPage authUser={authUser} />} />
+              <Route path="/user" element={<UserPage authUser={authUser} />} />
+              <Route path="/reservation" element={<ReservationPage authUser={authUser} />} />
             </>
           )}
           <Route path="/book" element={<BookPage authUser={authUser} />} />
