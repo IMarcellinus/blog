c82ff15d916d0bebdd73b71f45c441d36a4c9761
syaffaamf <amusyaffa32@gmail.com>
2024-07-28 19:39:47 +0700

    rename class to className in skeletonchartprodi

--- client/src/components/Skeleton/SkeletonChartProdi.jsx
+++ client/src/components/Skeleton/SkeletonChartProdi.jsx
@@ -5,13 +5,13 @@ const SkeletonChartProdi = () => {
     <div className="col-span-2 row-span-1 h-[550px] rounded-lg bg-white p-4 shadow-md lg:p-6 flex flex-col w-full">
       <div className="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4 mx-auto"></div>
       <div className="flex items-baseline space-x-4 justify-center">
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
-        <div class="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
+        <div className="w-14 bg-gray-200 rounded-t-lg h-96 dark:bg-gray-700"></div>
       </div>
       <div></div>
     </div>
